027b47863d3ad262f4827a1cdf91ca9a66b28dc2
G. Kayaalp <self@gkayaalp.com>
2015-10-22 17:44:37 +0300

    Add commentary

--- paper-theme.el
+++ paper-theme.el
@@ -29,6 +29,22 @@
 ;; 
 ;;; Commentary:
 ;;
+;; Paper is a  little, minimal emacs theme that is  meant to be simple
+;; and consistent.
+;;
+;; It  was first  intended  to resemble  the look  of  paper, but  has
+;; diverged from  that objective.   Still, though,  I keep  calling it
+;; Paper, as I like that name.
+;;
+;; Paper  uses a  small colour  palette  over all  the elements.   Org
+;; headings  are  specially  treated  with a  palette  of  equidistant
+;; colours.  The colours  and heading font sizes  are calculated using
+;; base and factor values which can be edited.  See source.
+
+;; It's most adapted for ELisp-Org users, as I'm one such user, though
+;; it  works fine  with Markdown,  Textile, Python,  JavaScript, Html,
+;; Diff, Magit, etc.
+;;
 ;;; Code:
 ;;
 
