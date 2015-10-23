2b943edab3040241c8d11c8595554c70d989995c
G. Kayaalp <self@gkayaalp.com>
2015-10-23 14:45:08 +0300

    Add snippet to comment for pretty org levels.

--- paper-theme.el
+++ paper-theme.el
@@ -40,10 +40,25 @@
 ;; headings  are  specially  treated  with a  palette  of  equidistant
 ;; colours.  The colours  and heading font sizes  are calculated using
 ;; base and factor values which can be edited.  See source.
-
+;;
 ;; It's most adapted for ELisp-Org users, as I'm one such user, though
 ;; it  works fine  with Markdown,  Textile, Python,  JavaScript, Html,
 ;; Diff, Magit, etc.
+;;
+;; The following snippet will modify org-level-* faces so that initial
+;; stars in  org headings are  hidden and  a Sans-serif font  is used.
+;; Because  the combination  of heading  font sizes  and colours  make
+;; levels  obvious, it  may be  considered superfluous  to have  stars
+;; indicating depth:
+;;
+;;   (setq org-hide-leading-stars nil)
+;;   (set-face-attribute
+;;    'org-hide nil
+;;    :height 0.1 :weight 'light :width 'extracondensed)
+;;   (dolist (face org-level-faces)
+;;     (set-face-attribute
+;;      face nil
+;;      :family "Sans Serif"))
 ;;
 ;;; Code:
 ;;
