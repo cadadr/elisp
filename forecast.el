2457878d6fc901b137ed65d1a3d9bdeb491773cf
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2017-01-07 18:14:09 +0300

    Set up the 0.6.0 release.

--- forecast.el
+++ forecast.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Göktuğ Kayaalp <self@gkayaalp.com>
 ;; Keywords: weather, forecast
-;; Version: 0.5.2
+;; Version: 0.6.0
 ;; URL: http://gkayaalp.com/emacs.html#forecast.el
 ;; Package-Requires: ((emacs "24.4"))
 ;;
@@ -148,6 +148,8 @@
 
 ;;; Changes:
 ;;
+;; v0.6.0, 07 Jan 2017
+;;   - Switch from forecast.io to darksky.net, their new service.
 ;; v0.5.2, 07 Jan 2017
 ;;   Thanks to Kaushal Modi for these patches:
 ;;   - Fix data displayed in mirrored fashion in Upcoming graph.
