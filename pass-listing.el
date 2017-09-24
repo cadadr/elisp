b5955cc3af601acee5d79ae55cb259789b0e950f
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2017-09-24 23:21:21 +0300

    pass-listing: Require password-store.

--- pass-listing.el
+++ pass-listing.el
@@ -6,6 +6,7 @@
 ;; Maintainer: Göktuğ Kayaalp <self@gkayaalp.com>
 ;; Version: 0.1.0
 ;; Keywords: unix
+;; Package-Requires: ((password-store . "0.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
