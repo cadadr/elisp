5e148ff6486af693e2c9a4d7ad70509ec5221249
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-02-02 04:18:51 +0300

    Typo.

--- org-variable-pitch.el
+++ org-variable-pitch.el
@@ -22,7 +22,7 @@
 
 ;;; Commentary:
 
-;; Variable-pitch support for org-mode.  This minor mode enabled
+;; Variable-pitch support for org-mode.  This minor mode enables
 ;; ‘variable-pitch-mode’ in the current Org-mode buffer, and sets some
 ;; particular faces up so that they are are rendered in fixed-width
 ;; font.  Also, indentation, list bullets and checkboxes are displayed
